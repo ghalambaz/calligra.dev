e076ee4ede7e8b62285c0eb78be7a9f3e2347f67
Mohammad Ghalambaz <aghalambaz@gmail.com>
2026-06-13 12:43:16 +0200

    deploy: 2026-06-13T10:43:16Z

--- content-modules.mjs
+++ content-modules.mjs
@@ -1,4 +1,11 @@
 
 export default new Map([
+["src/content/blogs/astro-vs-nextjs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-vs-nextjs.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/astro-vs-gatsby.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-vs-gatsby.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/introduction-to-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fintroduction-to-astro.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/css-grid-mastery.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fcss-grid-mastery.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/astro-component.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-component.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/the-essence-of-love.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fthe-essence-of-love.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/typescript-in-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Ftypescript-in-astro.mdx&astroContentModuleFlag=true")],
 ["src/content/blogs/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fhello-world.mdx&astroContentModuleFlag=true")]]);
 		
